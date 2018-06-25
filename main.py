d5e7b5fe9497b3223cd0d9a9c502bcc1e7a24804
Sirisha Marthy <sirisham@amazon.com>
2018-06-24 23:43:19 -0700

    Added new template newdonation and modified base.jinja2 for navigation elements and view route created for newdonation in main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donor, Donation
 
 app = Flask(__name__)
 
@@ -15,7 +15,19 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/newdonation', methods=["GET", "POST"])
+def new_donation():
+    if request.method == "POST":
+        #user = User.select().where(User.name == request.form['name']).get()
+        donor = Donor.select().where(Donor.name == request.form['name']).get()
+        print("donor is {}".format(donor))
+        donation = request.form['donation']
+        donation = Donation(value=donation, donor=donor)
+        donation.save()
+        return redirect(url_for('all'))
+    else:
+        return render_template('newdonation.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
